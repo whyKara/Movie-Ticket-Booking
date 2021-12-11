ec7bdcd95f8964a113dd34c94e95371ec77424c1
starix2436 <75304097+starix2436@users.noreply.github.com>
2021-12-11 15:24:17 +0530

    add: pages

--- index.php
+++ index.php
@@ -20,7 +20,18 @@
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+<?php
+	<div class="card" style="width: 18rem;">
+	<img src="..." class="card-img-top" alt="...">
+	<div class="card-body">
+		<h5 class="card-title">Card title</h5>
+		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		<a href="#" class="btn btn-primary">Go somewhere</a>
+	</div>
+	</div>
+?>
+
+<!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
