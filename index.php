2477c7eaf1b79989ac7cd07fe09ad4e34a7a1a20
starix2436 <75304097+starix2436@users.noreply.github.com>
2021-12-16 13:23:55 +0530

    add: navbar , footer file

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
   </head>
   <body>
     <h1>Hello, world!</h1>
@@ -20,16 +19,7 @@
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
-<?php
-	<div class="card" style="width: 18rem;">
-	<img src="..." class="card-img-top" alt="...">
-	<div class="card-body">
-		<h5 class="card-title">Card title</h5>
-		<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		<a href="#" class="btn btn-primary">Go somewhere</a>
-	</div>
-	</div>
-?>
+
 
 <!-- Optional JavaScript; choose one of the two! -->
 
---
+++ pages/booking.php
@@ -0,0 +1,1 @@
+<!-- Tikcet booking page -->
